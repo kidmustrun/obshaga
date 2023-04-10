d93bf5af16379653a8a6aed4ce4b468051a64fc8
Irina <parakerta@mail.ru>
2023-04-10 15:44:24 +0300

    layout for message card

---
+++ components/Message.vue
@@ -0,0 +1,43 @@
+<template data-app>
+<v-card class="message p-3">
+    <v-row
+              align="center"
+              no-gutters
+            >
+            <v-col cols="auto">
+    <v-avatar
+            class="ratio ratio-1x1 me-3"
+            width="100%"
+            height="auto"
+            max-width="50px"
+          >
+            <v-img
+              :src="src"
+              alt="Avatar"
+              class="ratio ratio-1x1"
+              width="100%"
+              height="auto"
+              max-width="50px"
+          /></v-avatar>
+        </v-col>
+        <v-col>
+       <span>{{ message }}</span></v-col>
+        </v-row>
+</v-card>
+
+</template>
+
+
+<script>
+export default {
+    name: 'Message',
+    props: ['src', 'message']
+}
+</script>
+<style scoped>
+.message{
+    display: inline-block;
+    max-width: 50vw;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -77,115 +77,7 @@
           </button>
         </div>
         <div class="messages">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolore
-        similique in, minus, alias quaerat delectus quibusdam sed assumenda odio
-        distinctio, et eius perferendis nesciunt. Molestias eveniet nihil neque
-        laboriosam, sequi ipsam amet qui excepturi a dolor cupiditate aperiam?
-        Harum pariatur reiciendis nesciunt iste velit vel eius animi esse
-        provident, in eveniet. Sunt temporibus corporis quod quas voluptatum
-        expedita mollitia obcaecati dicta modi odio! Ipsam repellendus
-        laboriosam distinctio quam inventore maiores, autem quas, deserunt, ipsa
-        laborum quaerat tempora? Magni odit unde ipsam dolore illum asperiores
-        nesciunt consequatur beatae, rerum voluptate fugiat excepturi?
-        Cupiditate totam vel quasi aut aliquam molestiae quae inventore eaque.
-        Atque porro, eligendi repellat qui voluptatum reprehenderit consequuntur
-        ipsam maxime aliquid delectus, incidunt accusantium ad mollitia
-        consectetur, quia velit neque? Omnis eveniet nostrum nihil asperiores
-        corporis aliquid repellendus, dolor natus voluptate ullam ad in tenetur
-        architecto optio, incidunt voluptatem necessitatibus deserunt
-        laudantium. Maxime veniam vitae nam dolorem fugit. Molestiae,
-        necessitatibus molestias, dolor obcaecati amet aliquid inventore error
-        est nulla ipsum explicabo nam eveniet dignissimos veritatis? Quos velit
-        consequuntur praesentium eveniet, veritatis iste itaque, suscipit ipsum
-        molestias dolore ratione modi! Odit natus rerum deserunt illo voluptas
-        culpa aut. Obcaecati iste aut doloremque, tenetur, optio perferendis
-        eaque nam ducimus blanditiis unde inventore dignissimos quasi cum
-        deserunt tempore hic quod! Quibusdam laborum beatae, quo quas odit non
-        fugit rerum eligendi ad quam, ipsum, dolores nostrum at perspiciatis
-        ducimus corrupti tempora similique corporis commodi a. Ex, minima
-        facilis rerum doloremque ea recusandae ipsum esse, eligendi facere enim
-        a delectus fugiat eaque neque, sit suscipit aperiam sint optio.
-        Laboriosam error aperiam eum vero quis ipsa soluta ducimus placeat dicta
-        earum voluptatum maiores animi nisi delectus quos, eveniet debitis
-        repellat asperiores repellendus culpa minima! Pariatur magni possimus
-        recusandae unde? Nostrum, quasi esse unde provident inventore nobis
-        dicta ullam aliquid. Quae, tenetur deserunt, sunt quasi quas porro eius
-        cumque suscipit possimus eos fuga! Necessitatibus velit nesciunt facere
-        rerum similique esse, maiores excepturi officia sequi recusandae aliquid
-        dicta assumenda ullam repudiandae laboriosam commodi accusantium
-        tempora. Exercitationem quod dignissimos eveniet blanditiis quos, harum
-        quisquam voluptas itaque aut eaque vitae vero esse ex, corrupti
-        voluptatem, dicta ab! Hic voluptatum sunt sint consequuntur, enim
-        molestiae iusto debitis odio quae maiores nobis ab impedit omnis quos
-        beatae quis id labore eius quo maxime autem accusantium? Saepe a omnis
-        quasi hic adipisci! Odit sint omnis quod ea, consequuntur deleniti
-        distinctio commodi non eligendi fugiat explicabo ipsa. Atque consectetur
-        optio, assumenda doloribus, officia a, eos sint obcaecati perferendis
-        mollitia sequi similique repellendus possimus nostrum fugiat? Pariatur
-        nesciunt alias harum eius sint. Non maiores hic quae fugit consequatur,
-        eius animi iusto ad nemo aliquid, libero consectetur natus. Nemo
-        cupiditate doloribus consequuntur voluptate. Laudantium unde, alias
-        beatae perferendis vel officiis quia harum magnam aspernatur nam rem
-        eaque animi amet voluptas quas sint eos eum accusamus atque doloribus
-        accusantium repudiandae sit minus. Minus vero explicabo voluptatibus
-        quisquam sint eligendi, quos quo natus mollitia voluptatem, earum fuga
-        rerum dolore esse provident architecto, deleniti illo? Quo aliquam
-        eligendi suscipit vitae fuga beatae maxime magni laudantium, aspernatur
-        rem. Repellendus voluptas soluta consequatur in vero doloremque nihil
-        dicta tenetur architecto id! Nobis, sed, cumque quisquam ab tempora
-        incidunt non deleniti voluptatum et repudiandae magni, maiores est
-        necessitatibus consequuntur aperiam hic iusto. Dignissimos fuga illum
-        quaerat tenetur ratione, placeat quisquam. Impedit, quae officiis.
-        Recusandae vero fugit eligendi, eveniet quasi dolores obcaecati atque
-        porro illum, perferendis molestiae quos nisi laudantium numquam corrupti
-        veritatis molestias adipisci ipsam modi maiores dolorum! Adipisci,
-        delectus aspernatur iste ducimus ullam explicabo sint quas fugit
-        inventore fuga eaque, molestias est iusto suscipit, error distinctio
-        corrupti dolorem libero sit ipsam consequuntur! Fugiat perspiciatis,
-        accusamus ullam explicabo accusantium corporis eaque, facere animi
-        quisquam porro dolore nesciunt neque hic! Ab rem quas neque vero
-        obcaecati nam. Enim voluptatibus voluptate necessitatibus sunt numquam
-        est iure labore hic adipisci, ad molestiae odio quia repellendus
-        eveniet. A adipisci vitae non illum quibusdam nemo tenetur, molestias
-        ipsam voluptas. Eos nihil mollitia beatae alias harum totam doloremque
-        minima magnam error, repellendus at corrupti ex odio autem accusantium
-        necessitatibus quibusdam nostrum voluptatibus iste fugiat quidem fugit
-        aliquid eius aut! Minima aspernatur veritatis dolorum, voluptatum
-        explicabo quis repellendus voluptates ratione doloribus, accusantium
-        exercitationem deserunt beatae voluptatem provident fuga ad consequuntur
-        dicta expedita, cumque reiciendis fugiat dolore magni? Quia libero sed
-        cupiditate porro dignissimos error debitis, quod sunt corporis, eligendi
-        corrupti adipisci possimus aspernatur odio quos blanditiis ipsum sequi
-        laborum. Consectetur eligendi a veniam dolores fugiat nisi animi quas
-        voluptatem fuga. Delectus tempore quasi aut, ducimus beatae corporis
-        assumenda ipsam reprehenderit totam voluptatum vitae quo quia nihil
-        harum illum accusamus dolorum consequuntur ex eius doloremque! Provident
-        voluptatum illo alias illum culpa sequi cum nobis eum delectus, sunt
-        perferendis vero accusamus omnis fugiat dicta in eaque error corporis
-        tempora optio impedit inventore aperiam dignissimos. Harum eligendi
-        fugit, corporis eius ex laboriosam earum non modi aut quod sequi tenetur
-        sapiente, enim quasi, libero velit molestias rem. Iure labore corrupti
-        neque beatae odio vero, architecto qui quam exercitationem a quae,
-        commodi sapiente! Autem non dolores harum obcaecati saepe laborum
-        quibusdam, reprehenderit at eligendi laboriosam modi repellat molestias
-        voluptates ut cum itaque minima earum suscipit alias error. At explicabo
-        consequuntur beatae ullam quidem dicta repellendus nesciunt modi,
-        corporis, reprehenderit illo repudiandae ad quia saepe voluptatibus
-        possimus sapiente odit nobis dolore tempore veritatis praesentium? Illum
-        non nostrum rerum corrupti et quaerat, totam architecto quidem numquam
-        illo. Id placeat temporibus consequatur cumque ipsam, atque quos, ipsa
-        consequuntur fuga a eaque nulla perspiciatis? Quia soluta architecto
-        magni voluptas nam, eum, nobis ad culpa debitis sapiente minus incidunt
-        perspiciatis quod mollitia maiores labore. Tempora tempore voluptatum
-        eligendi est? Recusandae rem repellendus ad aut inventore! Et quae, quos
-        natus autem, pariatur debitis magnam eligendi alias molestias, possimus
-        assumenda! Nam nostrum nobis eum. Consequatur fuga sequi, voluptates
-        similique reiciendis minima ex sapiente, architecto quod optio modi
-        magnam pariatur autem, corporis repudiandae! Exercitationem fugiat animi
-        pariatur quisquam commodi, praesentium minima odit, quam similique
-        voluptatem hic accusantium quis necessitatibus libero illum soluta ea
-        sit fugit quo inventore! Blanditiis neque suscipit eos, corrupti
-        praesentium quisquam voluptatem error commodi vitae quo. Pariatur.
+        <Message :src="user.photo_url" :message="'Lorem ipsumfb afjsfhusa jfbjsakf sdfsfaf sahdghasfgahgsf fdgasf fhf hdfghasfi dfhbhjfd hfd fhfsagfa'"/>
       </div>
         <div class="position-fixed fixed-bottom input-message" >
           <input v-model="chat" class="form-control" />
@@ -277,7 +169,7 @@ export default {
         about: 'Ищу друзей по программированию и буду рада новым знакомствам',
         course: 4,
         photo_url:
-          'https://sun1-27.userapi.com/impg/m_uavZ5HEm_Oa_jBKbBVhXRHKBjioxe-R-E9zg/5di_I3euAIY.jpg?size=1620x2160&quality=95&sign=641eeefc25e16874383e3364e0991550&type=album',
+          'https://sun9-24.userapi.com/impg/OVhLzZ5n0hyQdXbSJpmzE5NMtoHZarSHTC4lZg/IySh5Ym3iRM.jpg?size=1620x2160&quality=95&sign=b52b9205acbbf5045e1f4963594ff01d&type=album',
         faculty: 'Информационных технологий',
         filters: [
           {
@@ -320,6 +212,7 @@ export default {
     openUserChat: false,
     width: 0,
     chat: '',
+    user:{},
   }),
   created() {
     if (process.browser) window.addEventListener('resize', this.updateWidth)
@@ -337,9 +230,11 @@ export default {
       if (this.openUserChat && this.width > 768) this.chatOpen = true
     },
     openUserChatMethod(id) {
-      this.messages = this.users.find((user) => user.id === id).messages
-      this.openUserChat = true
-      if (this.width <= 768) this.chatOpen = false
+      this.user = this.users.find((user) => user.id === id)
+      this.messages = this.user.messages;
+      this.openUserChat = true;
+      if (this.width <= 768) this.chatOpen = false;
+      
     },
     scrollToDown() {
       const el = this.$el.querySelector('#chat')
@@ -350,7 +245,7 @@ export default {
   },
   watch: {
     openUserChat() {
-      setTimeout(this.scrollToDown, 10)
+      setTimeout(this.scrollToDown, 0)
     },
   },
 }
@@ -414,6 +309,7 @@ h1 {
   background-color: #f5f5f5;
   height: calc(100vh - 64px) !important;
   overflow: auto;
+  width: calc(100% - 256px)
 }
 .button {
   top: 64px;
@@ -425,7 +321,7 @@ h1 {
   font-weight: 600;
 }
 .messages{
-  margin-top: 32px;
+  margin-top: 16px;
   margin-bottom: 32px;
 }
 .input-message{
@@ -434,6 +330,9 @@ h1 {
 @media screen and (max-width: 768px) {
   .openUserChat, .button, .input-message {
     left: 0;
+  }
+  .openUserChat{
+    width: 100%;
   }
 }
 </style>
